c7aa5ae739cab17b0c4d4892c38bbe5dc7b6e625
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2025-06-24 14:00:14 +0200

    remove uneeded tests and examples

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,7 @@
-using ArchGDAL: length
-using BenchmarkTools: haskey
 using GraphMLReader
 using MetaGraphs
-using JSON
 using BenchmarkTools
-using StaticGraphs
-using Graphs
-
 using Test
+using Printf
 
 include("test_readfile.jl")
-include("test_staticgraphs.jl")
